5180df92310ef9dfd0879f9576c148fe41d571db
Julianne Kuettner <julianne.kuettner@thenobleacademy.org>
2018-08-09 15:55:53 +0000

    automatically backed up by learn

---
+++ 1D.rb
@@ -0,0 +1,12 @@
+one_direction = {"Zayne" => 25,
+                 "Liam" => 24,
+                 "Harry" => 24,
+                 "Niall" => 24
+}
+
+#Level 1
+#How can you print out all the members of 1D and their ages like the following:
+#Zayn is 25 years old 
+#Liam is 24 years old 
+
+#Level 2 How can you get the average 
