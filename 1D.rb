a63425d27fb179e5941595e8ddaf9e386f1480cf
Julianne Kuettner <julianne.kuettner@thenobleacademy.org>
2018-08-09 16:01:35 +0000

    automatically backed up by learn

--- 1D.rb
+++ 1D.rb
@@ -1,13 +1,20 @@
-one_direction = {"Zayne" => 25,
-                 "Liam" => 24,
-                 "Harry" => 24,
-                 "Niall" => 24
+one_direction = {
+  "Zayne" => 25,
+  "Liam" => 24,
+  "Harry" => 24,
+  "Niall" => 24
 }
 
 #Level 1
 #How can you print out all the members of 1D and their ages like the following:
 #Zayn is 25 years old 
 #Liam is 24 years old 
+
+one_direction.each do [name, age]
+
+ puts one_direction
+
+end
 
 #Level 2 How can you get the average age of the members of 1D?
 
